362ef9848ede752a649a1bb8d2fef6819f2e0ff3
italoeduardo96@hotmail.com <italoeduardo96@hotmail.com>
2020-02-15 12:03:27 -0300

    configurar projeto com as devidas dependecias

---
+++ challenge-merchant-api/src/main/java/br/com/cabal/challengemerchantapi/ChallengeMerchantApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.cabal.challengemerchantapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeMerchantApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeMerchantApiApplication.class, args);
+	}
+
+}
